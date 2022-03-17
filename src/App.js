e1cbdb5906bcacd233af72537b5bbdee357384d2
DysonBL <100944502+DysonBL@users.noreply.github.com>
2022-03-17 10:56:40 +0530

    files uploaded

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Register from './componentslogin/Register';
+import Screen from './componentslogin/Screen';
+
+const App = () => {
+    // const[row,setData]=useState([])
+    // useEffect(()=>{
+    //     fetch("https://jsonplaceholder.typicode.com/todos").then((res)=>res.json().then((res)=>setData(res)))
+    // },[])
+    // console.log("login")
+return (
+    <div>
+        {/* <h1>Basic Table</h1> */}
+        {/* {row &&<BasicTable data={row}/>} */}
+        {/* <Screen/> */}
+         <Register/>
+       
+         
+    </div>
+)
+}
+
+
+export default App;
